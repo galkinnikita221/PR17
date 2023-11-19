0fa020deb30f7cf11c5b293b71a7cb616b8f17fa
Nikita <galkinnikita228@gmail.com>
2023-11-19 17:19:37 +0500

    Добавьте файлы проекта.

---
+++ classes/Dish.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza_Galkin.classes
+{
+    public class Dish
+    {
+        public int id;
+        public int id_size;
+        public int size;
+        public int price;
+        public int wes;
+
+        public int countOrder;
+        public bool orders;
+    }
+}
